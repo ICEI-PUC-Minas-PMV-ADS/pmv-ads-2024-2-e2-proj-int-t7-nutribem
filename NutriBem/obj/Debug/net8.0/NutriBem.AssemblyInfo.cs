50299bf4650deb6f2d46b506e270c81366084159
GuilhermePucMail <guilherme.salviano@sga.pucminas.br>
2024-10-25 22:13:59 -0300

    algumas alterações em pacientes e nutricionistas

--- NutriBem/obj/Debug/net8.0/NutriBem.AssemblyInfo.cs
+++ NutriBem/obj/Debug/net8.0/NutriBem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NutriBem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e65d7d267655bc08b006c9217b1dc0e6253e2f6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7649f88b11e5d4890ce1d667ea1c972732347d38")]
 [assembly: System.Reflection.AssemblyProductAttribute("NutriBem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NutriBem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
