7ffdc3ae577be8714d610ee9b8760bd855da1298
Joel-Onofre <165732924+JoelPuc@users.noreply.github.com>
2024-11-16 18:35:13 -0300

    assembly

--- NutriBem/obj/Debug/net8.0/NutriBem.AssemblyInfo.cs
+++ NutriBem/obj/Debug/net8.0/NutriBem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NutriBem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a249c97b170ebabe6e4d0f45438dbb6334bcd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f6971c04fe1ffd6df7e58db0f012a4408418c84")]
 [assembly: System.Reflection.AssemblyProductAttribute("NutriBem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NutriBem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
