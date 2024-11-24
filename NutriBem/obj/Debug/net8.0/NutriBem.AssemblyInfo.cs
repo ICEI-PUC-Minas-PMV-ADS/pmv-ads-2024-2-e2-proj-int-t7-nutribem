30021407982b461a7d55680747faa52399c7a90f
GuilhermePucMail <guilherme.salviano@sga.pucminas.br>
2024-11-24 12:23:10 -0300

    pushing features of staging

--- NutriBem/obj/Debug/net8.0/NutriBem.AssemblyInfo.cs
+++ NutriBem/obj/Debug/net8.0/NutriBem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NutriBem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+844fcea071e6db5407de5224015a47e15c3f6679")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23742b439e3ee9633f0289a2ba1f193cca60ee3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NutriBem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NutriBem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
