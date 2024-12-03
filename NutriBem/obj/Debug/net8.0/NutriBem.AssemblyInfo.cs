6c1a6ab73e8d8048baa86ebf9196a67c185595e8
AnaDunningham <acdmseixas@sga.pucminas.br>
2024-12-02 22:29:39 -0300

    Front-End Telas restantes

--- NutriBem/obj/Debug/net8.0/NutriBem.AssemblyInfo.cs
+++ NutriBem/obj/Debug/net8.0/NutriBem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NutriBem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a980ceddecb7be4f1c3dd77e0797c9b2ada392d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5481737d4169bebe9a34cddb6558bea411f7aaa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NutriBem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NutriBem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
